f565a07a4028921dfa6eec00957ecd460055dee0
hulu8004 <tom8004@hotmail.com>
2015-12-12 17:44:10 -0500

    Fixed the column issue with the article seeder.

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -13,7 +13,7 @@ class ArticlesTableSeeder extends Seeder
     {
         DB::table('pweb_articles')->insert([
             'title' => 'Welcome',
-            'value' => '<p>Congratulations on successfully installing your PW Web! Browse around and get a feel for everything! If you have any questions or issues please don\'t hesitate to post them on <a href="https://github.com/huludinid/pw-web">github!</a></p>',
+            'content' => '<p>Congratulations on successfully installing your PW Web! Browse around and get a feel for everything! If you have any questions or issues please don\'t hesitate to post them on <a href="https://github.com/huludinid/pw-web">github!</a></p>',
             'category' => 'other'
         ]);
     }
