00739e4ac00d444e979c28a36343166192ffd96f
hulu8004 <tom8004@hotmail.com>
2015-12-15 23:55:20 -0500

    Finished the services and fixed up the shop and some more minor fixes.

---
+++ app/Http/Controllers/Front/RankingController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class RankingController extends Controller
+{
+    //
+}
