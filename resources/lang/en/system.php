099d4fda9a79804eb03339e79757ad25e7e588fe
hulu8004 <tom8004@hotmail.com>
2016-01-03 12:55:24 -0500

    Fixed a language line.

--- resources/lang/en/system.php
+++ resources/lang/en/system.php
@@ -29,7 +29,7 @@ return [
         'new' => 'New',
         'notice' => [
             'title' => 'Pre-Release',
-            'body' => 'We don\'t recommend you use this version on a live server since it\'s still in development. If you receive any errors while using these versions please create an issue with the prefix <b>[:tag]</b>.'
+            'body' => 'We don\'t recommend you use this version on a live server since it\'s still in development. If you receive any errors while using this version please create an issue with the prefix <b>[:tag]</b>.'
         ]
     ]
 
