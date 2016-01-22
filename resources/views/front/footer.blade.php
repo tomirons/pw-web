1d11ca41aee29b7c00c0c74796e8beca49f8172b
hulu8004 <tom8004@hotmail.com>
2016-01-21 21:26:28 -0500

    Changed the footer text.

--- resources/views/front/footer.blade.php
+++ resources/views/front/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- BEGIN INNER FOOTER -->
 <div class="page-footer">
-    <div class="container"> Support the developers.
+    <div class="container"> Support the developers of the panel.
         <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=R9FB8E4X5EYVC" target="_blank">Donate!</a>
     </div>
 </div>
