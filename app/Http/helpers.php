7786404b0358e246de8715e50cf659342bce4c54
Harris Marfel <hrace009@gmail.com>
2021-11-26 16:02:43 +0700

    Change $days and $hours variable at function makeTime($time) that make A non well formed numeric value in the result

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -2,13 +2,13 @@
 
 function makeTime( $time )
 {
-    $days = floor( $time / 86400 );
-    $days = $days > 0 ? ( $days > 1 ) ? $days . " " . trans( 'ranking.time.days' ) : $days . " " . trans( 'ranking.time.day' ) : '';
+    $days = floor($time / 86400);
+    $TextDays = $days > 0 ? ($days > 1) ? $days . " " . trans('ranking.time.days') : $days . " " . trans('ranking.time.day') : '';
 
-    $hours = ceil( ceil( $time - ( $days * 86400 ) ) / 3600 );
-    $hours = $hours > 1 ? ( $hours > 1 ) ? $hours . " " . trans( 'ranking.time.hours' ) : $hours . " ". trans( 'ranking.time.hour' ) : ' > 1 ' . trans( 'ranking.time.hour' );
+    $hours = ceil(ceil($time - ($days * 86400)) / 3600);
+    $TextHours = $hours > 1 ? ($hours > 1) ? $hours . " " . trans('ranking.time.hours') : $hours . " " . trans('ranking.time.hour') : ' > 1 ' . trans('ranking.time.hour');
 
-    $time = $days . ' ' . $hours;
+    $time = $TextDays . ' ' . $TextHours;
 
     return $time;
 }
